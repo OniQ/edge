f2a5c9aba34c5b5d734d2eb08904b4023cbad5a3
OniQ <oniq.eg@gmail.com>
2015-04-25 18:53:38 +0300

    Toolbar button tooltips auto append by button name

--- scripts/app/directives/edgeConfig.js
+++ scripts/app/directives/edgeConfig.js
@@ -7,50 +7,57 @@ define(['edgeDirectives'], function(edgeDirectives){
             templateUrl: "/edge/templates/panels/configPanel.html",
             controller: function($scope, $element, $attrs) {
                 $scope.configuration = {
-                    title: "test title",
-                    level: 20,
-                    a: 'a',
-                    b: 'b',
-                    C: 'c',
-                    d: 'd',
-                    f: 'f',
-                    e: 'e',
-                    g: 'g',
-                    x: 'xxx',
-                    a1: 'a',
-                    b2: 'b',
-                    C3: 'c',
-                    d4: 'd',
-                    f1: 'f',
-                    e2: 'e',
-                    g3: 'g',
-                    x4: 'xxx',
-                    is: true,
-                    list: ['ok', 'go'],
-                    title1: "test title",
-                    level1: 20,
-                    a7: 'a',
-                    b7: 'b',
-                    C7: 'c',
-                    d7: 'd',
-                    f7: 'f',
-                    e7: 'e',
-                    g7: 'g',
-                    x7: 'xxx',
-                    a71: 'a',
-                    b72: 'b',
-                    C73: 'c',
-                    d74: 'd',
-                    f71: 'f',
-                    e72: 'e',
-                    g73: 'g',
-                    x74: 'xxx',
-                    i7s: true,
-                    l7ist: ['ok', 'go']
-                    //map: {
-                    //    o: 'pkpkpk'
-                    //}
+                        title: "test title",
+                        level: 20,
+                        a: 'a',
+                        b: 'b',
+                        C: 'c',
+                        d: 'd',
+                        f: 'f',
+                        e: 'e',
+                        g: 'g',
+                        x: 'xxx',
+                        a1: 'a',
+                        b2: 'b',
+                        C3: 'c',
+                        d4: 'd',
+                        f1: 'f',
+                        e2: 'e',
+                        g3: 'g',
+                        x4: 'xxx',
+                        is: true,
+                        list: ['ok', 'go'],
+                        title1: "test title",
+                        level1: 20,
+                        a7: 'a',
+                        b7: 'b',
+                        C7: 'c',
+                        d7: 'd',
+                        f7: 'f',
+                        e7: 'e',
+                        g7: 'g',
+                        x7: 'xxx',
+                        a71: 'a',
+                        b72: 'b',
+                        C73: 'c',
+                        d74: 'd',
+                        f71: 'f',
+                        e72: 'e',
+                        g73: 'g',
+                        x74: 'xxx',
+                        i7s: true,
+                        l7ist: ['ok', 'go']
+                        //map: {
+                        //    o: 'pkpkpk'
+                        //}
+                    };
+
+                $scope.synchConfig = function(opt){
+                    if (opt.type == 'number')
+                        opt.value = parseInt(opt.value);
+                    $scope.configuration[opt.name] = opt.value;
                 };
+
                 function getDropDownOptions(array){
                     if (!angular.isArray($scope.configuration[field]))
                         return;
@@ -63,43 +70,40 @@ define(['edgeDirectives'], function(edgeDirectives){
                     return array;
                 }
 
-                $scope.fields = [];
+                $scope.$watch('configuration', function(newVal, oldVal){
+                    if (newVal){
+                        $scope.fields = [];
 
+                        for (field in $scope.configuration){
+                            if (angular.isArray($scope.configuration[field])){
+                                $scope.fields.push({
+                                    name: field,
+                                    type: 'array',
+                                    options: getDropDownOptions($scope.configuration[field]),
+                                    value: $scope.configuration[field]
+                                });
+                                var lastField = $scope.fields[$scope.fields.length-1];
+                                lastField.value = $scope.configuration[field][0].value;
+                                $scope.configuration[lastField.name] = lastField.value;
+                            }
+                            else {
+                                $scope.fields.push({
+                                    name: field,
+                                    type: typeof($scope.configuration[field]),
+                                    value: $scope.configuration[field]
+                                });
+                            }
+                        }
 
-                for (field in $scope.configuration){
-                    if (angular.isArray($scope.configuration[field])){
-                        $scope.fields.push({
-                            name: field,
-                            type: 'array',
-                            options: getDropDownOptions($scope.configuration[field]),
-                            value: $scope.configuration[field]
-                        });
-                        var lastField = $scope.fields[$scope.fields.length-1];
-                        lastField.value = $scope.configuration[field][0].value;
-                        $scope.configuration[lastField.name] = lastField.value;
+                        var columnsCount = Math.ceil($scope.fields.length / 4.0);
+                        var minimum = Math.min(4, columnsCount);
+                        $scope.colClass = 'col-xs-' + 12 / minimum;
+                        $scope.columns = [];
+                        var size = $scope.fields.length/columnsCount;
+                        while ($scope.fields.length > 0)
+                            $scope.columns.push($scope.fields.splice(0, size));
                     }
-                    else {
-                        $scope.fields.push({
-                            name: field,
-                            type: typeof($scope.configuration[field]),
-                            value: $scope.configuration[field]
-                        });
-                    }
-                }
-
-                $scope.synchConfig = function(opt){
-                    if (opt.type == 'number')
-                        opt.value = parseInt(opt.value);
-                    $scope.configuration[opt.name] = opt.value;
-                };
-
-                var columnsCount = Math.ceil($scope.fields.length / 4.0);
-                var minimum = Math.min(4, columnsCount);
-                $scope.colClass = 'col-xs-' + 12 / minimum;
-                $scope.columns = [];
-                var size = $scope.fields.length/columnsCount;
-                while ($scope.fields.length > 0)
-                    $scope.columns.push($scope.fields.splice(0, size));
+                });
             }
         };
     });
--- scripts/app/directives/edgeToolbar.js
+++ scripts/app/directives/edgeToolbar.js
@@ -2,12 +2,24 @@
  * Created by OniQ on 22/04/15.
  */
 define(['edgeDirectives'], function(edgeDirectives){
-    edgeDirectives.directive('edgeToolbar', function($rootScope){
+    edgeDirectives.directive('edgeToolbar', function($compile, $timeout){
         return {
             templateUrl: "/edge/templates/panels/toolbar.html",
-            controller: function($scope, $element, $attrs) {
-                $scope.makeDisplayResizable = function(){
-                    $rootScope.isResizable = !$rootScope.isResizable;
+            compile: function compile(tElement, tAttrs, transclude) {
+                return {
+                    pre: function preLink(scope, iElement, iAttrs, controller) {
+                        var buttons = iElement.context.children;
+                        angular.forEach(buttons, function (button) {
+                            if (button.attributes['name']) {
+                                var name = button.attributes['name'].nodeValue;
+                                var tooltipWrapper = "<div tooltip='" + name + "'" +
+                                    " tooltip-placement='right' style='display: inline-block' tooltip-popup-delay='700'>"
+                                    + "</div>";
+                                angular.element(button).wrap(tooltipWrapper);
+                                $compile(iElement.contents())(scope);
+                            }
+                        });
+                    }
                 }
             }
         };
