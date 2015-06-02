f71f87c0ea3bcc188860adf50056b02ab660df6a
OniQ <oniq.eg@gmail.com>
2015-06-02 13:44:13 +0300

    default code

--- scripts/app/controllers/main1Controller.js
+++ scripts/app/controllers/main1Controller.js
@@ -14,12 +14,17 @@ define(['edgeCtrl'], function(edgeCtrl){
                     status: "notLoaded",
                     type: "sprite"
                 },
+                sound:{
+                    name: "Comet.mp3",
+                    status: "notLoaded",
+                    type: "audio"
+                },
                 speed: 3,
                 width: 80,
                 height: 80,
                 z: 5,
-                x: 485,
-                y: 155,
+                x: 500,
+                y: 400,
                 animation: 0
             };
 
@@ -42,7 +47,47 @@ define(['edgeCtrl'], function(edgeCtrl){
 
             var edgeStorage = localStorageService.get('consoleCode');
             if (!edgeStorage)
-                localStorageService.set('consoleCode', "var fn = edge.functionParameters;");
+                localStorageService.set('consoleCode', "var fn = edge.functionParameters;\n" +
+                    "if (edge.firstRun){\n" +
+                    "    var objects = edge.getByField('name', 'rocket');\n" +
+                    "    fn.rocket = objects[0]; \n" +
+                    "    fn.rocket.speed = 2; \n" +
+                    "    fn.counter = 0;\n" +
+                    "    fn.state = 0;\n" +
+                    "    fn.rocket.x = 500;\n" +
+                    "    fn.rocket.y = 400;\n" +
+                    "    fn.rocket.animation = 0;\n" +
+                    "    edge.playAudio(fn.rocket, 'sound');\n" +
+                    "}\n" +
+                    "fn.counter++;\n" +
+                    "switch(fn.state){\n" +
+                    "    case 0:\n" +
+                    "         fn.rocket.y -= fn.rocket.speed;\n" +
+                    "         break;\n" +
+                    "    case 1: fn.rocket.x -= fn.rocket.speed;\n" +
+                    "         break;\n" +
+                    "    case 2: fn.rocket.y += fn.rocket.speed;\n" +
+                    "         break;" +
+                    "    case 3: fn.rocket.x += fn.rocket.speed;\n" +
+                    "         break;\n" +
+                    "}\n" +
+                    "if (fn.counter > 120){\n" +
+                    "   if (fn.state === 3 && !fn.boost){\n" +
+                    "       fn.boost = true;\n" +
+                    "       edge.playAudio(fn.rocket, 'sound')\n" +
+                    "   }\n" +
+                    "   else if (fn.state === 3 && fn.boost)\n" +
+                    "       fn.boost = false;\n" +
+                    "   fn.state = (fn.state+1) % (4);\n" +
+                    "   fn.counter = 0;\n" +
+                    "   fn.rocket.animation = fn.state;\n" +
+                    "   if (fn.boost){\n" +
+                    "       fn.rocket.animation = fn.rocket.animation + 4;\n" +
+                    "       fn.rocket.speed = 3;\n" +
+                    "   }\n" +
+                    "   else\n" +
+                    "     fn.rocket.speed = 2;\n" +
+                    "}");
             $scope.unbind = localStorageService.bind($scope, 'consoleCode');
 
             var editor = ace.edit("editor");
@@ -57,9 +102,16 @@ define(['edgeCtrl'], function(edgeCtrl){
                 },
                 readOnly: true // false if this command should not apply in readOnly mode
             });
+            editor.commands.addCommand({
+                name: 'save',
+                bindKey: {win: 'Ctrl-S',  mac: 'Command-S'},
+                exec: function(editor) {
+                    $scope.saveSrc();
+                },
+                readOnly: true
+            });
             editor.setValue($scope.consoleCode, -1);
             $scope.runSrc = function(){
-                //var session = editor.getSession();
                 var code = editor.getValue();
                 edge.executeFunction(code);
             };
